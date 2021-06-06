1ec3f8bd71a4bf6dcbe7a991bd3d9a200922178d
NataliaKhokhlova <76100365+NataliaKhokhlova@users.noreply.github.com>
2021-06-06 11:36:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,19 +5,12 @@ int prioritet (char value) {
   switch (value) {
       case '(':
       return 0;
-      break;
       case ')':
       return 1;
-      break;
       case '+':
-      return 2;
-      break;
       case '-':
       return 2;
-      break;
       case '*':
-      return 3;
-      break;
       case '/':
       return 3;
       break;
@@ -26,76 +19,64 @@ int prioritet (char value) {
       }
   }
 std::string infx2pstfx(std::string inf) {
- std::string pfx;
-  int i = 0;
-  char chv = inf[i];
-  char arr = 0;
-  TStack <char> stackChar;
-  while (chv) {
-    int priority;
-    priority = prior(chv);
-    if (priority > -1) {
-      if ((priority == 0 || priority > prior(arr) ||
-           stackChar.isEmpty()) && chv != ')') {
-        if (stackChar.isEmpty())
-          arr = chv;
-        stackChar.push(chv);
-        }
-      else if (chv == ')') {
-        while (stackChar.get() != '(') {
-          pfx.push_back(stackChar.get());
-          pfx.push_back(' ');
-          stackChar.pop();
-          }
-        stackChar.pop();
-        if (stackChar.isEmpty())
-          arr = 0;
-        }
-      else {
-        while (!stackChar.isEmpty() &&
-               prior(stackChar.get()) >= priority) {
-          pfx.push_back(stackChar.get());
-          pfx.push_back(' ');
-          stackChar.pop();
-  }
-        if (stackChar.isEmpty())
-          arr = chv;
-        stackChar.push(chv);
-        }
+  TStack<char> stack;
+  std::string pfx;
+  for (int i = 0; i < inf.length(); i++) {
+    if ((inf[i] >= '0') && (inf[i] <= '9')) {
+      r += inf[i];
+      r += ' ';
+      } else if (inf[i] == '(') {
+      stack.push(inf[i]);
+      } else if (p(inf[i]) > p(stack.get()) || stack.isEmpty()) {
+      stack.push(inf[i]);
+      } else if (inf[i] == ')') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        r += stack.get();
+        r += ' ';
+        stack.pop();
       }
-    else {
-      pfx.push_back(chv);
-      pfx.push_back(' ');
+      if (stack.get() == '(') {
+        stack.pop();
+      }
+      } else {
+      while (!stack.isEmpty() && p(stack.get()) >= p(inf[i])) {
+        r += stack.get();
+        r += ' ';
+        stack.pop();
+      }
+      stack.push(inf[i]);
+    }
+  }
+  while (!stack.isEmpty()) {
+    r += stack.get();
+    r += ' ';
+    stack.pop();
+  }
+  while (r[r.length() - 1] == ' ') {
+    r = r.substr(0, r.length()-1);
+  }
+  return r;
 }
-    chv = inf[++i];
-    }
-  while (!stackChar.isEmpty()) {
-    pfx.push_back(stackChar.get());
-    pfx.push_back(' ');
-    stackChar.pop();
-    }
-  pfx.erase(pfx.end() - 1, pfx.end());
-  return pfx;
-  }
 int eval(std::string pst) {
- std::string tstr;
-TStack<int> stack2;
-for (int i = 0; i < pst.length(); i++) {
-  if (pst[i] >= '0' && pst[i] <= '9') {
-    tstr = pst[i];
-    stack2.push(pst[i] - '0');
-    }
-  else if (pst[i] != ' ') {
-    int second = stack2.get();
-    stack2.pop();
-    int first = stack2.get();
-    stack2.pop();
-    if (pst[i] == '*') stack2.push(first * second);
-    else if (pst[i] == '/') stack2.push(first / second);
-    else if (pst[i] == '+') stack2.push(first + second);
-    else {
-      stack2.push(first - second);
+TStack<int> stack;
+  for (int i = 0; i < pst.length(); i++) {
+    if ((pst[i] >= '0') && (pst[i] <= '9')) {
+      stack.push(pst[i] - '0');
+      } else if (pst[i] != ' ') {
+      int second = stack.get();
+      stack.pop();
+      int first = stack.get();
+      stack.pop();
+      if (pst[i] == '-') {
+        stack.push(first - second);
+        } else if (pst[i] == '+') {
+        stack.push(first + second);
+        } else if (pst[i] == '*') {
+        stack.push(first * second);
+        } else {
+        stack.push(first / second);
       }
     }
-  return stack2.get();
   }
+  return stack.get();
+}
