a55a3d9a9ea28a93209e9eeb46795a72dc3df730
NataliaKhokhlova <76100365+NataliaKhokhlova@users.noreply.github.com>
2021-06-06 21:28:07 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,95 +1,32 @@
-#include <string>
-#include "tstack.h"
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
-int prior(char ch) {
-    switch (ch) {
-    case '(':
-        return 0;
-    case ')':
-        return 1;
-    case '+':
-        return 2;
-    case '-':
-        return 2;
-    case '*':
-        return 3;
-    case '/':
-        return 3;
-    default:
-        return -1;
-    }
-}
+const int size = 100;
 
-std::string infx2pstfx(std::string inf) {
- std::string pfx;
-    int i = 0;
-    char ch = inf[i];
-    char top = 0;
-    TStack <char> stackChar;
-    while (ch) {
-        int priority;
-        priority = prior(ch);
-
-        if (priority > -1) {
-            if ((priority == 0 || priority > prior(top) ||
-                stackChar.isEmpty()) && ch != ')') {
-                if (stackChar.isEmpty())
-                    top = ch;
-                stackChar.push(ch);
-            } else if (ch == ')') {
-                while (stackChar.get() != '(') {
-                    pfx.push_back(stackChar.get());
-                    pfx.push_back(' ');
-                    stackChar.pop();
-                }
-             stackChar.pop();
-                if (stackChar.isEmpty())
-                    top = 0;
-            } else {
-                while (!stackChar.isEmpty() &&
-                       prior(stackChar.get()) >= priority) {
-                    pfx.push_back(stackChar.get());
-                    pfx.push_back(' ');
-                    stackChar.pop();
-                }
-             if (stackChar.isEmpty())
-                    top = ch;
-                stackChar.push(ch);
-            }
-        } else {
-            pfx.push_back(ch);
-            pfx.push_back(' ');
-        }
-
-        ch = inf[++i];
-    }
- while (!stackChar.isEmpty()) {
-        pfx.push_back(stackChar.get());
-        pfx.push_back(' ');
-        stackChar.pop();
-    }
- pfx.erase(pfx.end() - 1, pfx.end());
-    return pfx;
-}
-
-int eval(std::string pst) {
- std::string tstr;
-  TStack<int> stack2;
-  for (int i = 0; i < pst.length(); i++) {
-    if (pst[i] >= '0' && pst[i] <= '9') {
-      tstr = pst[i];
-      stack2.push(pst[i] - '0');
-    } else if (pst[i] != ' ') {
-      int second = stack2.get();
-      stack2.pop();
-      int first = stack2.get();
-      stack2.pop();
-      if (pst[i] == '*') stack2.push(first * second);
-      else if (pst[i] == '/') stack2.push(first / second);
-      else if (pst[i] == '+') stack2.push(first + second);
-      else
-        stack2.push(first - second);
-    }
-  }
-  return stack2.get();
-}
+template<typename type>
+class TStack {
+ private:
+     type arr[size];
+     int top;
+ public:
+     TStack() : top(-1) { }
+     type get() const {
+        return arr[top];
+     }
+     bool isEmpty() const {
+        return top == -1;
+     }
+     bool isFull() const {
+        return top == size - 1;
+     }
+     void pop() {
+        if (top >= 0)
+          top--;
+     }
+     void push(type item) {
+       if (top < size - 1)
+         arr[++top] = item;
+     }
+};
+#endif  // INCLUDE_TSTACK_H_
